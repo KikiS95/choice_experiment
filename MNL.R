b99ae2feaa10e32519aca2fb0b5b2455c94f60b0
kota <k.mameta78@gmail.com>
2019-07-16 09:33:49 +0900

    Update MNL.R

--- MNL.R
+++ MNL.R
@@ -1,8 +1,12 @@
-// > Dataset1 <- read.csv("/Users/kiran/Desktop/DI998 Dissertation/DataAnalysis/choice_data1.csv") 
+library(mlogit)
+setwd("~/Dropbox/Shared_NIES/Shared_KiranAgrPJ/DataAnalysis")
+Dataset1 <- read.csv("choice_data1.csv",header=T) 
 #imported the file from desktop and created Dataset as Dataset1
-// > data_1 <-mlogit.data(Dataset1, shape = "long", choice = "choice", varying = c(4:11), alt.levels = c("1", "2", "3"))
+data_1 <-mlogit.data(Dataset1, shape = "long", choice = "choice", varying = c(4:11), alt.levels = c("1", "2", "3"))
 #created data1 object to format the data using mlogit.data()
-// > mod1 <-mlogit(choice ~ alt + frog + bird + fish + location1 + cultivation1 + price, data= data1)
+names(data_1)
+data_1 <- data_1[,-c(9,12)]
+mod1 <-mlogit(choice ~ asc + frog + bird + satoyama + organic + price|0|0, data= data_1)
 #this code is not working and comes up with the following error
 #Error in solve.default(H, g[!fixed]) : 
 # system is computationally singular: reciprocal condition number = 1.55655e-22

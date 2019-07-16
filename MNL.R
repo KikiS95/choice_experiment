1e3534760c71b8e712505b22e1763aa5aa963166
kota <k.mameta78@gmail.com>
2019-07-16 11:25:59 +0900

    Update MNL.R

--- MNL.R
+++ MNL.R
@@ -6,9 +6,10 @@ data_1 <-mlogit.data(Dataset1, shape = "long", choice = "choice", varying = c(4:
 #created data1 object to format the data using mlogit.data()
 names(data_1)
 data_1 <- data_1[,-c(9,12)]
-mod1 <-mlogit(choice ~ fish + frog + bird + location1 + cultivation1 + price|0|0, data= data_1)
+mod1 <-mlogit(choice ~ fish + frog + bird + location1 + cultivation1 + price|0|1, data= data_1)
 #this code is not working and comes up with the following error
 #Error in solve.default(H, g[!fixed]) : 
 # system is computationally singular: reciprocal condition number = 1.55655e-22
 
 summary(mod1)
+
